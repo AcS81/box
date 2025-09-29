2dd4ac911f65fbb2a6d7d8c91947803742795b73
ahmet can <csoruklu@hotmail.com>
2025-09-29 01:34:51 +0100

    Initial Commit

---
+++ box/boxApp.swift
@@ -0,0 +1,17 @@
+//
+//  boxApp.swift
+//  box
+//
+//  Created by AcS on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct boxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
